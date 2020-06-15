de9158ed5ced06d64fe6f82dcf53f8e593d8cd71
Jaedon <jaedonjaezel@gmail.com>
2020-06-15 15:43:40 -0700

    set up screen

--- snakes.py
+++ snakes.py
@@ -6,3 +6,10 @@ import random
 #score
 score = 0
 high_score = 0
+
+#set up screen
+wn = turtle.Screen()
+wn.title("A Snake Game By Jaedon")
+wn.bgcolor("green")
+wn.setup(width = 600, height = 600)
+wn.tracer(0)
